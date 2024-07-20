6fb7b63b64e4ac62500e0f7ba45ae40c86a58409
Devanshi Sahu <devanshioffc@gmail.com>
2024-07-20 20:27:19 +0530

    first commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,13 @@
+import {useState} from "react"
+
+export default function TodoInput(props) {
+    const {handleAddTodos, todoValue, setTodoValue} = props
+
+    return (
+        <header>
+            <input value={todoValue} onChange={(e) => {setTodoValue(e.target.value)}} placeholder="Enter To-do" />
+            <button onClick={() => {handleAddTodos(todoValue)
+                setTodoValue('')}}>Add</button>
+        </header>
+    )
+}
